083d6524b9f3ceb463ca0bb7417c7199c77c45b8
Cat-bst <bestc7959@student.faytechcc.edu>
2025-01-27 16:06:19 +0000

    added }

---
+++ m1t2_best.cpp
@@ -0,0 +1,18 @@
+// CSC-134
+// M1T2
+// Cat Best
+// January 27, 2025
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "My name is Cat Best."; 
+    cout << endl; 
+    cout << "My major is information technology."; 
+    cout << endl;
+    cout << "I like watching anime and listening to music.";
+    cout << endl;
+    return 0;
+}
