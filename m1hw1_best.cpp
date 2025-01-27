906bb9a5daf90f5a9592c6a90aae5f6321c46548
Cat-bst <bestc7959@student.faytechcc.edu>
2025-01-27 16:46:43 +0000

    fixed errors and spaces

--- m1hw1_best.cpp
+++ m1hw1_best.cpp
@@ -17,10 +17,17 @@ int main() {
 
 
     cout << "A movie I really like is " << name;
-    << endl;
+    cout << endl;
     cout << "It came out in " << movie;
     cout << " and earned " << gross; 
     cout << " million dollars. " << endl;
     cout << "Obi-Wan Kenobi is a young apprentice Jedi knight under the tutelage of Qui-Gon Jinn; Anakin Skywalker, who will later father Luke Skywalker and become known as Darth Vader, is just a 9-year-old boy. When the Trade Federation cuts off all routes to the planet Naboo, Qui-Gon and Obi-Wan are assigned to settle the matter.";
-    << endl;
+    cout << endl;
+    cout << "One thing I like about the movie is that it gives background information on Anakins character.";
+    cout << endl; 
+    cout << "A quote from the movie I like is, \"Fear is a path to the dark side. Fear leads to anger. Anger leads to hate. Hate leads to suffering.\" ";
+    cout << endl; 
+    cout << "Here are a couple more quotes from the movie I find interesting: \"You can't stop change any more than you can stop the suns from setting.\"";
+    cout << "\"I was not elected to watch my people suffer and die while you discuss this invasion in a committee.\" Padme is one of my favorite characters, so I like this quote a lot.";
+    cout << endl;
 }
