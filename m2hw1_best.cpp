3669294ea5a3a9c8588cfc871b1d6e9271feb410
Cat-bst <bestc7959@student.faytechcc.edu>
2025-02-05 16:30:34 +0000

    no errors

---
+++ m2hw1_best.cpp
@@ -0,0 +1,31 @@
+// CSC 134
+// M2HW1 - Silver
+// Cat Best
+// 2/5/2025
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ 
+    string name = "Cat";
+    int startingbalance = 100;
+    double amountofdeposit =25;
+    int amountwithdrawl = 50;
+    int accountnum = 1031;
+    cout << "What is your name? " << endl;
+    cin >> name;
+    cout << "What is your starting balance?" << endl;
+    cin >> startingbalance;
+    cout << "How much would you like to deposit?" << endl; 
+    cin >> amountofdeposit;
+    cout << "How much would you like to withdrawl?" << endl;
+    cin >> amountwithdrawl;
+
+    cout << "The name on the account is " << name << endl;
+    cout << "The account number is " << accountnum << endl;
+    cout << "Your financial account balance is $" << startingbalance + amountofdeposit - amountwithdrawl << endl;
+
+    return 0;
+}
---
+++ question3.cpp
@@ -0,0 +1,31 @@
+// CSC 134
+// M2HW1 - Silver
+// Cat Best
+// 2/5/2025
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ 
+    
+    int numofpizzas = 3;
+    int slicesperpizza =25;
+    int numofvisitors = 50;
+   
+
+    cout << "How many pizzas do you want to order? " << endl;
+    cin >> numofpizzas;
+    cout << "How many slices do you want each pizza to have?" << endl;
+    cin >> startingbalance;
+    cout << "How many visitors are coming?" << endl; 
+    cin >> amountofdeposit;
+   
+
+    cout << "The name on the account is " << name << endl;
+    cout << "The account number is " << accountnum << endl;
+    cout << "Your financial account balance is $" << startingbalance + amountofdeposit - amountwithdrawl << endl;
+
+    return 0;
+}
