3e2c378a72e421bf268f379ce49c4fedb2cc58a6
Cat-bst <bestc7959@student.faytechcc.edu>
2025-02-10 15:25:24 +0000

    deleted gap

--- m2lab1.cpp
+++ m2lab1.cpp
@@ -1,22 +1,45 @@
+// CSC 134
+// M2LAB1
+// 2/10/2025
+// Cat Best
+
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
-   cout << "Total width: ";
-   int total_width;
-   cin >> total_width;
-   cout << "Tile width: ";
-   int tile_width;
-   cin >> tile_width;
+   const double COST_PER_CUBIC_FOOT = 0.23;
+   const double CHARGE_PER_CUBIC_FOOT = 0.5; 
 
-   int number_of_pairs = (total_width - tile_width) / (2 * tile_width);
-   int number_of_tiles = 1 + 2 * number_of_pairs;
-   double gap = (total_width - number_of_tiles * tile_width) / 2.0;
+   double length,
+   width,
+   height,
+   volume,
+   cost,
+   charge,
+   profit;
 
-   cout << "Number of tiles: " << number_of_tiles << endl;
-   cout << "Gap at each end: " << gap << endl;
+   cout <<"Enter the dimensions of the crate (in feet):\n";
+   cout << "Length: ";
+   cin >> length; 
+   cout << "Width: ";
+   cin >> width;
+   cout << "Height: ";
+   cin >> height;
+
+  volume = length * width * height;
+  cost = volume * COST_PER_CUBIC_FOOT;
+  charge = volume * CHARGE_PER_CUBIC_FOOT;
+  profit = charge - cost; 
+
+   cout << "The volume of the crate is: ";
+   cout << volume << " cubic feet.\n";
+   cout << "Cost to build: $" << cost << endl;
+   cout << "Charge to customer: $" << charge << endl;
+   cout << "Profit: $" << profit << endl; 
+   
    
    return 0;
 }
