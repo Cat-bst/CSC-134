566d8cd36c68cfa5dc7aae174107eec4c98a6849
Cat-bst <bestc7959@student.faytechcc.edu>
2025-02-26 16:02:49 +0000

    ok

--- m3hw1q2.cpp
+++ m3hw1q2.cpp
@@ -3,3 +3,22 @@
 // Cat Best
 // 2/26/2025
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    double mealPrice = 5.99;
+    double taxRate = 0.08;
+    double taxAmount;
+    double total;
+
+taxAmount = taxRate * mealPrice;
+total = mealPrice + taxAmount;
+
+cout << "Thank you for dining with us." << endl;
+cout << "Your meal total is $" << mealPrice << endl;
+cout << "The tax is $" << taxAmount << endl;
+cout << "Your total is $" << total << endl;
+cout << "Please come again." << endl;
+
+}
